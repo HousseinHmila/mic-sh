804cf3b8de5492a91c92cb897d77ab4bdf6e8de3
unknown <housseinhmila@gmail.com>
2022-02-28 23:40:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const box=document.getElementById("box");
+const box1=document.getElementById("box1");
+const tit=document.querySelector("h1")
+box.onclick=function(){
+box.style.display="none";
+tit.style.display="block"
+box1.style.display="block";
+}
